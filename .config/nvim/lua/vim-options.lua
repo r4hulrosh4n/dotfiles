143723010c366ca3d6600a23787ab28d5026ea94
Rahul Roshan <r4hulrosh4n@gmail.com>
2024-10-09 06:03:45 +0530

    changes

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -8,21 +8,21 @@ vim.g.background = "light"
 vim.opt.swapfile = false
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
-
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
--- vim.wo.number = true
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+vim.wo.number = true
 vim.wo.relativenumber = true
 
-vim.api.nvim_set_keymap('n', '<A-7>', ':bprevious<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<A-0>', ':bnext<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<A-n>', ':set relativenumber!<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "tk", ":blast<enter>", {noremap=false})
-vim.api.nvim_set_keymap("n", "tj", ":bfirst<enter>", {noremap=false})
-vim.api.nvim_set_keymap("n", "th", ":bprev<enter>", {noremap=false})
-vim.api.nvim_set_keymap("n", "tl", ":bnext<enter>", {noremap=false})
-vim.api.nvim_set_keymap("n", "td", ":bdelete<enter>", {noremap=false})
+vim.api.nvim_set_keymap("n", "tk", ":blast<enter>", { noremap = false })
+vim.api.nvim_set_keymap("n", "tj", ":bfirst<enter>", { noremap = false })
+vim.api.nvim_set_keymap("n", "th", ":bprev<enter>", { noremap = false })
+vim.api.nvim_set_keymap("n", "tl", ":bnext<enter>", { noremap = false })
+vim.api.nvim_set_keymap("n", "td", ":bdelete<enter>", { noremap = false })
+vim.api.nvim_set_keymap("n", "vs", ":vsplit<enter>", { noremap = false })
+vim.api.nvim_set_keymap("n", "<A-n>", ":set relativenumber! number!<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<C-y>", '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>r', ':%s///g<Left><Left><Left>', { noremap = true, silent = true }) -- Replace command
